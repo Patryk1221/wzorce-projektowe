55f9a57218872fef5c89aa5f1a20828063aa3417
Patryk1221 <162486885+Patryk1221@users.noreply.github.com>
2025-11-09 17:27:30 +0100

    Add files via upload

---
+++ Prototype/Ork.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace OrkPrototype
+{
+    internal class Ork
+    {
+        private static readonly Random Rng = new Random();
+
+        public int Age { get; set; }
+        public int Strength { get; set; }
+        public int Speed { get; set; }
+
+        public Ork DeepClone()
+        {
+            string json = JsonConvert.SerializeObject(this);
+            Ork clone = JsonConvert.DeserializeObject<Ork>(json);
+            return clone;
+        }
+
+        public Ork CloneWithRandomStrength(int min = 0, int max = 300)
+        {
+            Ork clone = DeepClone();
+            clone.Strength = Rng.Next(min, max);
+            return clone;
+        }
+
+        public override string ToString()
+        {
+            return $"Siła: {Strength}, Wiek: {Age}, Szybkość: {Speed}";
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,22 @@
+﻿using OrkPrototype;
+
+List<Ork> squad = new();
+
+var baseOrk = new Ork
+{
+    Age = 100,
+    Strength = 200,
+    Speed = 50
+};
+
+squad.Add(baseOrk);
+
+for (int i = 0; i < 4; i++)
+{
+    squad.Add(baseOrk.CloneWithRandomStrength());
+}
+
+foreach (var ork in squad)
+{
+    Console.WriteLine($"Cześć! {ork}");
+}
